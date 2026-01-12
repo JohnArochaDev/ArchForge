c5e9423249828a5f78ccc6cf5d735a4df0304b49
John Arocha <jparocha777@gmail.com>
2026-01-11 20:37:50 -0700

    added new cmds

--- scripts/update-keybinds.sh
+++ scripts/update-keybinds.sh
@@ -8,27 +8,47 @@ CONFIG_FILE="$HOME/.config/hypr/bindings.conf"
 OLD_TERMINAL_CMD="SUPER, RETURN"
 NEW_TERMINAL_CMD="SUPER, T"
 
+TILING_CONFIG_FILE="$HOME/.local/share/omarchy/default/hypr/bindings/tiling-v2.conf"
+
+NEW_CLOSE_WINDOW_CMD="bindd = SUPER, W, Close window, killactive"
+
 # Check if config file exists
 if [ ! -f "$CONFIG_FILE" ]; then
   echo "Error: COnfig file not found at $CONFIG_FILE"
   exit 1
 fi
 
-# Create backup of original config
-BACKUP_FILE="{$CONFIG_FILE}.backup"
-cp "$CONFIG_FILE" "$BACKUP_FILE"
-echo "Backup created at: $BACKUP_FILE"
-
 # Replace the binding with sed
 
-# CHanges terminal keybind to SUPER + T
+# changes terminal keybind to SUPER + T
 sed -i "s/bindd = SUPER, RETURN, Terminal/bindd = SUPER, T, Terminal/" "$CONFIG_FILE"
 
-# Check if change was made
+# changes SUPER + T keybind to empty in omarchy tiling-v2.conf
+sed -i '8d' "$TILING_CONFIG_FILE"
+
+# SUPER + W keybind to empty in omarchy tiling-v2.conf
+sed -i '2d' "$TILING_CONFIG_FILE"
+# Add new SUPER + W cmd to bindings.conf
+#
+
+# Check if change was made: terminal NEW_TERMINAL_CMD
 if grep -q "s/bindd = SUPER, T, Terminal" "$CONFIG_FILE"; then
   echo "Successfully changed terminal hotkey to SUPER + T"
 else
   echo "Warning: Could not find the binding to change"
+fi
+
+if grep -q "bindd = SUPER, T, Toggle window floating/tiling, toggleFloating," "$CONFIG_FILE"; then
+  echo "Warning: Couldnt find keybinding"
+else
+  echo "Successfully removed previous T keybinding"
+fi
+
+if grep -q "$NEW_CLOSE_WINDOW_CMD" "$CONFIG_FILE"; then
+  echo "Binding already exists!"
+else
+  echo "$NEW_CLOSE_WINDOW_CMD" >> "$CONFIG_FILE"
+  echo "Successfully changed window close hotkey to SUPER + Q"
 fi
 
 echo ""
